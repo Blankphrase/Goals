41bf2c2e8f0a08b66640a9874d8d27152be95e37
Blankphrase <ckasera6486@gmail.com>
2018-05-30 11:26:27 +0300

    finishing application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { NgProgressModule } from '@ngx-progressbar/core';
+import { NgProgressHttpModule } from '@ngx-progressbar/http';
 
 import { AppComponent } from './app.component';
 import { GoalComponent } from './goal/goal.component';
@@ -8,6 +11,10 @@ import { GoalDetailsComponent } from './goal-details/goal-details.component';
 import { StrikethroughDirective } from './strikethrough.directive';
 import { DateCountPipe } from './date-count.pipe';
 import { GoalFormComponent } from './goal-form/goal-form.component';
+import {AlertsService} from './alert-service/alerts.service';
+import { AboutComponent } from './about/about.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { RoutingModule } from './routing/routing.module'
 
 @NgModule({
   declarations: [
@@ -16,13 +23,19 @@ import { GoalFormComponent } from './goal-form/goal-form.component';
     GoalDetailsComponent,
     StrikethroughDirective,
     DateCountPipe,
-    GoalFormComponent
+    GoalFormComponent,
+    AboutComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RoutingModule,
+    HttpClientModule,
+    NgProgressModule.forRoot(),
+    NgProgressHttpModule,
   ],
-  providers: [],
+  providers: [AlertsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -2,35 +2,55 @@ import { Component, OnInit } from '@angular/core';
 import {Goal} from '../goal';
 import {Goals} from '../goals';
 import {GoalService} from '../goals/goal.service';
+import {AlertsService} from '../alert-service/alerts.service';
+import {HttpClient} from '@angular/common/http';
+import {Quote} from '../qoute-class/quote';
+import { Router } from '@angular/router';
+import { QuoteRequestService } from '../quote-http/quote-request.service'
+
 
 @Component({
   selector: 'app-goal',
   templateUrl: './goal.component.html',
-  providers:[GoalService],
+  providers:[GoalService,QuoteRequestService],
   styleUrls: ['./goal.component.css']
 })
 export class GoalComponent implements OnInit {
 
-
+  quote:Quote;
   goals:Goal[];
+  alertService:AlertsService;
 
- addNewGoal(goal){
-   let goalLength = this.goals.length;
-   goal.id=goalLength+1;
-   goal.completeDate = new Date(goal.completeDate)
-   this.goals.push(goal)
- }
-    completeGoal(isComplete,index){
-        if (isComplete){
-            this.goals.splice(index,1);
-            }
-            }
+   addNewGoal(goal){
+     let goalLength = this.goals.length;
+     goal.id=goalLength+1;
+     goal.completeDate = new Date(goal.completeDate)
+     this.goals.push(goal)
+   }
+
+   goToUrl(id){
+     this.router.navigate(['/goals',id])
+   }
+   deleteGoal(index){
+     let toDelete = confirm('Are you sure you want to delete ${this.goals[index].name}')
+     if(toDelete){
+       this.goals.splice(index,1)
+       this.alertService.alertMe("Goal has been deleted")
+     }
+   }
+
     toogleDetails(index){
         this.goals[index].showDescription = !this.goals[index].showDescription;
     }
-  constructor(GoalService:GoalService) {
-  this.goals=goalService.getGoals() }
-  ngOnInit() {
-  }
+
+    constructor(goalService:GoalService,alertService:AlertsService,private quoteService:QuoteRequestService, private router:Router) {
+      this.goals=goalService.getGoals();
+      this.alertService = alertService;
+     }
+
+    ngOnInit() {
+        this.quoteService.quoteRequest()
+        this.quote = this.quoteService.quote
+    }
 
 }
